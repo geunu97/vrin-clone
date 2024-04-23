204c8c25ac7cb739259a51d5cea853c3feb5d376
geunu <olo90632951@gmail.com>
2024-04-23 22:07:07 +0900

    style: header class 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,20 +8,20 @@ export default function Header() {
   return (
     <div className={styles.header}>
       <div className={styles.header__inner}>
-        <div className={styles.header__logo}>
+        <div className={styles.header__inner__logo}>
           <Link href="/">
             <LogoSvg color="white" />
           </Link>
         </div>
-        <ul className={styles.header__menu}>
-          <li className={styles.header__menu__price}>
+        <ul className={styles.header__inner__menu}>
+          <li className={styles.header__inner__menu__price}>
             <Link href="/pricing">가격정책</Link>
           </li>
-          <li className={styles.header__menu__kor}>KOR</li>
-          <li className={styles.header__menu__login}>
+          <li className={styles.header__inner__menu__kor}>KOR</li>
+          <li className={styles.header__inner__menu__login}>
             <Link href="/login">로그인</Link>
           </li>
-          <li className={styles.header__menu__start}>
+          <li className={styles.header__inner__menu__start}>
             <Link href="/register">무료로 시작하기</Link>
           </li>
         </ul>
