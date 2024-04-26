32acc625cf223c81ac3621a8b3491f64085b3f4b
geunu <olo90632951@gmail.com>
2024-04-27 02:52:03 +0900

    style: 600px 이하 모바일 헤더 UI 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import styles from '@/styles/header.module.scss';
 import LogoSvg from '@/assets/svg/Logo';
 import ArrowTopSvg from '@/assets/svg/ArrowTop';
+import MenuSvg from '@/assets/svg/Menu';
+import CloseSvg from '@/assets/svg/Close';
 import ModalSelect from '@/components/ModalSelect';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
@@ -11,6 +13,7 @@ export default function Header() {
   const router = useRouter();
   const { t, i18n } = useTranslation('common');
   const [isOpenLanguageModal, setIsOpenLanguageModal] = useState(false);
+  const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false);
   const languages = [
     {
       label: 'KOR',
@@ -28,45 +31,86 @@ export default function Header() {
   };
 
   return (
-    <div className={styles.header}>
-      <div className={styles.header__inner}>
-        <div className={styles.header__inner__logo}>
-          <Link href="/">
-            <LogoSvg color="white" />
-          </Link>
+    <>
+      <div className={styles.header}>
+        <div className={styles.header__inner}>
+          <div className={styles.header__inner__logo}>
+            <Link href="/">
+              <LogoSvg color="white" />
+            </Link>
+          </div>
+          <ul className={styles.header__inner__menu}>
+            <li className={styles.header__inner__menu__price}>
+              <Link href="/pricing">{t('sentence.pricing_policy')}</Link>
+            </li>
+            <li className={styles.header__inner__menu__language}>
+              <button
+                className={styles.header__inner__menu__language__button}
+                onClick={() => setIsOpenLanguageModal(!isOpenLanguageModal)}
+              >
+                {languages.find((language) => language.value === i18n.language).label}
+              </button>
+              <span className={styles.header__inner__menu__language__arrow}>
+                <ArrowTopSvg color="rgb(111, 117, 123)" />
+              </span>
+              {isOpenLanguageModal && (
+                <ModalSelect
+                  setOpen={setIsOpenLanguageModal}
+                  className={styles.header__inner__menu__language__modal}
+                  options={languages}
+                  value={i18n.language}
+                  onChange={handleLanguageChange}
+                />
+              )}
+            </li>
+            <li className={styles.header__inner__menu__login}>
+              <Link href="/login">{t('word.login')}</Link>
+            </li>
+            <li className={styles.header__inner__menu__start}>
+              <Link href="/register">{t('sentence.start_for_free')}</Link>
+            </li>
+          </ul>
         </div>
-        <ul className={styles.header__inner__menu}>
-          <li className={styles.header__inner__menu__price}>
-            <Link href="/pricing">{t('sentence.pricing_policy')}</Link>
-          </li>
-          <li className={styles.header__inner__menu__language}>
-            <button
-              className={styles.header__inner__menu__language__button}
-              onClick={() => setIsOpenLanguageModal(!isOpenLanguageModal)}
-            >
-              {languages.find((language) => language.value === i18n.language).label}
-            </button>
-            <span className={styles.header__inner__menu__language__arrow}>
-              <ArrowTopSvg color="rgb(111, 117, 123)" />
-            </span>
-            {isOpenLanguageModal && (
-              <ModalSelect
-                setOpen={setIsOpenLanguageModal}
-                className={styles.header__inner__menu__language__modal}
-                options={languages}
-                value={i18n.language}
-                onChange={handleLanguageChange}
-              />
-            )}
-          </li>
-          <li className={styles.header__inner__menu__login}>
-            <Link href="/login">{t('word.login')}</Link>
-          </li>
-          <li className={styles.header__inner__menu__start}>
-            <Link href="/register">{t('sentence.start_for_free')}</Link>
-          </li>
-        </ul>
       </div>
-    </div>
+
+      <div className={styles.header__mobile}>
+        <div className={styles.header__mobile__inner}>
+          <div className={styles.header__mobile__inner__main}>
+            <div className={styles.header__mobile__inner__main__left}>
+              <span
+                className={styles.header__mobile__inner__main__left__menu}
+                onClick={() => setIsOpenMobileMenu(!isOpenMobileMenu)}
+              >
+                {isOpenMobileMenu ? <CloseSvg color="white" /> : <MenuSvg color="white" />}
+              </span>
+              <Link href="/">
+                <LogoSvg color="white" />
+              </Link>
+            </div>
+            <Link href="/login">{t('word.login')}</Link>
+          </div>
+          {isOpenMobileMenu && (
+            <div className={styles.header__mobile__inner__menu}>
+              <Link href="/pricing">{t('sentence.pricing_policy')}</Link>
+              <div className={styles.header__mobile__inner__menu__language}>
+                <span
+                  className={`${styles.header__mobile__inner__menu__language__KO} ${i18n.language === 'ko' ? 'active' : ''}`}
+                  onClick={() => handleLanguageChange('ko')}
+                >
+                  KOR
+                </span>
+                <span className={styles.header__mobile__inner__menu__language__divider}></span>
+                <span
+                  className={`${styles.header__mobile__inner__menu__language__EN} ${i18n.language === 'en' ? 'active' : ''}`}
+                  onClick={() => handleLanguageChange('en')}
+                >
+                  ENG
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
